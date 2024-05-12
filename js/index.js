e1311728340048348bc606e00b418fcb756d5a58
Gustavo Rodolfo Paz <gusti.paz@gmail.com>
2024-05-12 18:54:35 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue
+createApp({
+    data(){
+        return {
+            mensaje: "probando VUE.js",
+            curso: "CAC full stack JAVA 2024 comision 24117",
+            alumno: "Gustavo Rodolfo Paz"
+        }
+    }
+}).mount('#app')
