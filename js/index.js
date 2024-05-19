a71ce618fb607cd2a7e61e295961527f34c203f7
guspaz0 <gusti.paz@gmail.com>
2024-05-19 18:02:38 -0300

    ok

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
 const { createApp } = Vue
 
-const navbar = {
-    template: `<p>Este es el header {{mensaje}}</p>`,
+const Navbar = {
+    template: `<navbar>
+        <img src="" alt="🌎"/>
+        <span>{{Titulo}} 🌎</span>
+        <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/login">Iniciar sesion</a></li>
+            <li><a href="/home">Home</a></li>
+        </ul>
+    </navbar>`,
     data(){
         return {
-            mensaje: "mensaje header"
+            Titulo: "API del clima"
         }
     },
 }
 const pie = {
     template: `<p>
-        Este es el footer {{mensaje}}
+        <span>{{titulo}}</span>
+        <span>{{curso}}</span>
+        <span>Autor: {{alumno}}</span>
     </p>`,
     data(){
         return {
-            mensaje: "mensaje footer"
+            titulo: "mensaje footer",
+            curso: "CAC full stack JAVA 2024 comision 24117",
+            alumno: "Gustavo Rodolfo Paz"
         }
     }
 }
@@ -23,13 +35,11 @@ const pie = {
 createApp({
     data(){
         return {
-            mensaje: "probando VUE.js",
-            curso: "CAC full stack JAVA 2024 comision 24117",
-            alumno: "Gustavo Rodolfo Paz"
+            mensaje: "Informacion meteorologica",
         }
     },
     components: {
         'pie': pie,
-        'navbar': navbar
+        'navbar': Navbar
     }
 }).mount('#app')
