0550bdf16a7953210f52756aca9537c9057b4fbe
guspaz0 <guspaz0>
2024-05-25 17:14:17 -0300

    fix validacion form login

--- js/components/login.vue.js
+++ js/components/login.vue.js
@@ -7,52 +7,53 @@ const login = {
                 <legend>
                     <label for="username">Nombre de usuario y/o Correo:</label>
                 </legend>
-                <input v-on:input="validateForm('username')" type="text" id="username" name="username"/>
+                <input v-model="username" type="text" id="username" name="username"/>
+                <small v-if="errors?.username" className="error">{{errors.username}}</small>
             </fieldset>
             <fieldset>
                 <legend>
                     <label for="password">Contraseña:</label>
                 </legend>
-                <input v-on:input="validateForm('password')" type="password" id="password" name="password"/>
+                <input v-model="password" type="password" id="password" name="password"/>
+                <small v-if="errors?.password" className="error">{{errors.password}}</small>
             </fieldset>
-            <input id="submit" name="submit" type="submit"/>
+            <label for="submit"></label>
+            <input id="submit" name="submit" type="submit" value="Enviar"/>
         </form>
-        <p><a href="./register.html">Registrate</a></p>
+        <p><a href="/register">Registrate</a></p>
     </div>
     </div>`,
     data(){
         return {
             errors: {},
+            username: '',
+            password: '',
+            form: {}
         }
     },
-    methods: {
-        validateForm(input){
-            const {username, password} = document.querySelector('form')
+    watch: {
+        username(value){
             let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (this.errors[input]) {
-                delete this.errors[input]
-                if (document.querySelector(`small#error${input}`)) document.querySelector(`small#error${input}`).remove()
-            }
-            switch(input) {
-                case 'username':
-                    if (username.value.length < 2) this.errors.username = "el nombre de usuario debe tener mas de 3 digitos"
-                    else if(!regex.test(username.value)) this.errors.username = "debe ser un email valido"
-                    break
-                case 'password':
-                    if (password.value.length == 0) this.errors.password = "el campo no puede estar vacio"
-                    break
-                default:
-                    break
-            }
-            if (this.errors[input]) {
-                let errorTag = `<small id="error${input}" class="error">${this.errors[input]}</small>`
-                document.querySelector(`input[name='${input}']`).insertAdjacentHTML('afterend', errorTag)
-            }
+            this.username = value
+            delete this.errors.username
+            delete this.form.username
+            if (this.username.length < 2) this.errors.username = "el nombre de usuario debe tener mas de 3 digitos"
+            else if(!regex.test(username.value)) this.errors.username = "debe ser un email valido"
+            if (!this.errors.username) this.form.username = value
         },
+        password(value){
+            this.password = value
+            delete this.errors.password
+            delete this.form.password
+            if (this.password.length == 0) this.errors.password = "el campo no puede estar vacio"
+            if(!this.errors.password) this.form.username = value
+        },
+    },
+    methods: {
         handleSubmit(e){
             e.preventDefault()
-            Array.from(document.querySelector('form')).forEach(input => this.validateForm(input.name));
             if (Object.keys(this.errors).length > 0) alert('corregir los errores del formulario')
+            else if (Object.keys(this.form).length == 0) alert('completar el formulario por favor')
             else alert('Sitio en construccion')
         }
     }
